a4e142593537fc35a96d1149cce5ce232ff12d07
idea4good <idea4good@outlook.com>
2019-07-19 23:46:02 +0800

    draw surfaces/layers

--- Graphic/main.js
+++ Graphic/main.js
@@ -1,26 +1,46 @@
 let layer1, layer2;
 let angle = 0;
+let step_layer = 0;
+let step_surface = 0;
 
 function setup() {
   layer1 = loadImage('assets/layer1.png')
   layer2 = loadImage('assets/layer2.png')
-  createCanvas(300, 300, WEBGL);
+  createCanvas(700, 600, WEBGL);
 }
 
 function drawLayer(layer, x, y, z){
   push();
   translate(x, y, z);
-  texture(layer);
+  rotateX(-1.2);
+  //texture(layer);
   plane(200, 200);
   pop();
 }
 
+function showLayers() {
+  drawLayer(layer1, step_layer, step_layer, -100);
+  drawLayer(layer2, -step_layer, -step_layer, -100);
+  if (step_layer < 25) {
+  	step_layer++;
+  }
+}
+
+function showSurface(){
+  if (step_surface > 220) {
+  	return true;
+  }
+  step_surface += 10;
+  drawLayer(layer1, 0, 0, -100);
+  drawLayer(layer2, -step_surface, 0, -100);
+  drawLayer(layer2, step_surface, 0, -100);
+  return false;
+}
+
 function draw() {
   background(0);
-  rotateX(-0.8);
-
-  drawLayer(layer1, 0, -100, -100);
-  drawLayer(layer2, 50, -200, -100);
-
+  if (showSurface()) {
+  	showLayers();
+  }
   angle += 0.05;
 }
