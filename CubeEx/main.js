3f05993a08f7fe2da01979a5141652b7add0d9a6
idea4good <idea4good@outlook.com>
2019-08-09 18:02:31 +0800

    CubeEx show both particle and wave

--- CubeEx/main.js
+++ CubeEx/main.js
@@ -1,52 +1,53 @@
 //////////////////////// WebAssembly Code ////////////////////////
+let instance = [null, null]
 let memoryStates = new WeakMap();
 let mode = 1
-let buffer = null;
+let buffer = null
 
-function syscall(instance, n, args) {
-  switch (n) {
-    default:
-      // console.log("Syscall " + n + " NYI.");
-      break;
-    case /* brk */ 45: return 0;
-    case /* writev */ 146:
-      return instance.exports.writev_c(args[0], args[1], args[2]);
-    case /* mmap2 */ 192:
-      // debugger;
-      const memory = instance.exports.memory;
-      let memoryState = memoryStates.get(instance);
-      const requested = args[1];
-      if (!memoryState) {
-        memoryState = {
-          object: memory,
-          currentPosition: memory.buffer.byteLength,
-        };
-        memoryStates.set(instance, memoryState);
-      }
-      let cur = memoryState.currentPosition;
-      if (cur + requested > memory.buffer.byteLength) {
-        const need = Math.ceil((cur + requested - memory.buffer.byteLength) / 65536);
-        memory.grow(need);
-      }
-      memoryState.currentPosition += requested;
-      return cur;
+function startWebAssembly(index, wasmFilePath, drawPixel) {
+  function syscall(instance, n, args) {
+    switch (n) {
+      default:
+        // console.log("Syscall " + n + " NYI.");
+        break;
+      case /* brk */ 45: return 0;
+      case /* writev */ 146:
+        return instance.exports.writev_c(args[0], args[1], args[2]);
+      case /* mmap2 */ 192:
+        // debugger;
+        const memory = instance.exports.memory;
+        let memoryState = memoryStates.get(instance);
+        const requested = args[1];
+        if (!memoryState) {
+          memoryState = {
+            object: memory,
+            currentPosition: memory.buffer.byteLength,
+          };
+          memoryStates.set(instance, memoryState);
+        }
+        let cur = memoryState.currentPosition;
+        if (cur + requested > memory.buffer.byteLength) {
+          const need = Math.ceil((cur + requested - memory.buffer.byteLength) / 65536);
+          memory.grow(need);
+        }
+        memoryState.currentPosition += requested;
+        return cur;
+    }
   }
-}
 
-function startWebAssembly(wasmFilePath, drawPixel) {
   let s = "";
   fetch(wasmFilePath).then(response =>
     response.arrayBuffer()
   ).then(bytes =>
     WebAssembly.instantiate(bytes, {
       env: {
-        __syscall0: function __syscall0(n) { return syscall(instance, n, []); },
-        __syscall1: function __syscall1(n, a) { return syscall(instance, n, [a]); },
-        __syscall2: function __syscall2(n, a, b) { return syscall(instance, n, [a, b]); },
-        __syscall3: function __syscall3(n, a, b, c) { return syscall(instance, n, [a, b, c]); },
-        __syscall4: function __syscall4(n, a, b, c, d) { return syscall(instance, n, [a, b, c, d]); },
-        __syscall5: function __syscall5(n, a, b, c, d, e) { return syscall(instance, n, [a, b, c, d, e]); },
-        __syscall6: function __syscall6(n, a, b, c, d, e, f) { return syscall(instance, n, [a, b, c, d, e, f]); },
+        __syscall0: function __syscall0(n) { return syscall(instance[index], n, []); },
+        __syscall1: function __syscall1(n, a) { return syscall(instance[index], n, [a]); },
+        __syscall2: function __syscall2(n, a, b) { return syscall(instance[index], n, [a, b]); },
+        __syscall3: function __syscall3(n, a, b, c) { return syscall(instance[index], n, [a, b, c]); },
+        __syscall4: function __syscall4(n, a, b, c, d) { return syscall(instance[index], n, [a, b, c, d]); },
+        __syscall5: function __syscall5(n, a, b, c, d, e) { return syscall(instance[index], n, [a, b, c, d, e]); },
+        __syscall6: function __syscall6(n, a, b, c, d, e, f) { return syscall(instance[index], n, [a, b, c, d, e, f]); },
         putc_js: function (c) {
           c = String.fromCharCode(c);
           if (c == "\n") {
@@ -63,23 +64,24 @@ function startWebAssembly(wasmFilePath, drawPixel) {
       }
     })
   ).then(results => {
-    instance = results.instance
-    instance.exports.main(mode)
+    instance[index] = results.instance
+    instance[index].exports.main(mode)
   }).catch(console.error);
 }
 
 //////////////////////// Cube Code ////////////////////////
-let instance = null;
 const cubeSize = 180;
 const gain = 1;
 const animationFrames = 200;
-let graphic;
+let graphic1, graphic2;
 
 function setup() {
   createCanvas(500, 500, WEBGL);
   angleMode(DEGREES);
-  graphic = createGraphics(240, 320);
-  startWebAssembly('libs/particle.wasm', drawPixelOnGraphic);
+  graphic1 = createGraphics(240, 320);
+  graphic2 = createGraphics(240, 320);
+  startWebAssembly(0, 'libs/particle.wasm', drawPixelOnGraphic1);
+  startWebAssembly(1, 'libs/wave.wasm', drawPixelOnGraphic2);
 }
 
 const faces = [
@@ -99,7 +101,8 @@ function draw() {
   rotateY(frameCount);
 
   if(progress == 1) {
-    updateGraphic();
+    updateGraphic(0);
+    updateGraphic(1);
   }
   
   faces.forEach((face, i) => {
@@ -115,23 +118,29 @@ function draw() {
   });
 }
 
-function updateGraphic() {
-  if(!instance){
+function updateGraphic(index) {
+  if(!instance[index]){
     return;
   }
 
-  var pointer = instance.exports.updateHelloParticle()
+  if (index == 0){
+    var pointer = instance[index].exports.updateHelloParticle()
+    var graphic = graphic1
+  } else{
+    var pointer = instance[index].exports.updateHelloWave()
+    var graphic = graphic2
+  }
 
   if(mode != 0){
     return;
   }
 
   if(buffer == null){
-    buffer = new Uint8Array(instance.exports.memory.buffer)
+    buffer = new Uint8Array(instance[index].exports.memory.buffer)
   }
   for(let y = 0; y < 320; y++) {
     for(let x = 0; x < 240; x++) {
-      graphic.stroke('rgb(' + buffer[pointer + 1] + ', ' + buffer[pointer + 2] + ', ' + buffer[pointer + 3] + ')');
+      graphic.stroke('rgb(' + buffer[pointer + 1] + ', ' + buffer[pointer + 2] + ', ' + buffer[pointer + 3] + ')')
       graphic.rect(x, y, 1, 1);
       pointer = pointer + 4;
     }
@@ -144,13 +153,31 @@ function textureFace(face, i, progress) {
   }
 
   if(i == 0 || i == 2) {
-    texture(graphic)
+    
   } else {
-    return fill(`rgb(${face[3]})`);
+    
+  }
+  switch(i){
+    case 0:
+    case 2:
+      texture(graphic1)
+    break;
+    case 1:
+    case 3:
+      texture(graphic2)
+    break;
+    default:
+      return fill(`rgb(${face[3]})`);
+    break;
   }
 }
 
-function drawPixelOnGraphic(x, y, color) {
-  graphic.stroke('rgb(' + ((color & 0xff0000) >> 16) + ', ' + ((color & 0xff00) >> 8) + ', ' + (color & 0xff) + ')');
-  graphic.rect(x, y, 1, 1);
+function drawPixelOnGraphic1(x, y, color) {
+  graphic1.stroke('rgb(' + ((color & 0xff0000) >> 16) + ', ' + ((color & 0xff00) >> 8) + ', ' + (color & 0xff) + ')')
+  graphic1.rect(x, y, 1, 1);
+}
+
+function drawPixelOnGraphic2(x, y, color) {
+  graphic2.stroke('rgb(' + ((color & 0xff0000) >> 16) + ', ' + ((color & 0xff00) >> 8) + ', ' + (color & 0xff) + ')')
+  graphic2.rect(x, y, 1, 1);
 }
